820c7f1c9aff5c4452703c520104f7be776b0f1b
fiokee <smleffiong@gmail.com>
2021-02-23 23:26:08 +0100

    broken our homepage into smaller components

---
+++ E-COMMERCE/react-shopping-cart/src/pages/hompage/homepage.components.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './homepag.styles.scss';
+import Directory from '../../components/directory/directory';
+const HomePage = () =>(
+
+    <div className="hompeage">
+    <Directory/>
+       
+    </div>
+)
+
+export default HomePage;
